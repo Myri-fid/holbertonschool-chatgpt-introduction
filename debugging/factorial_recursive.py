babf69707c7472323850dde02b83b8fc717697bd
Myriam <myriiamwayne@gmail.com>
2025-01-13 13:15:27 +0100

    factorial recersive

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
 import sys
 
-# Function description:
-# The `factorial` function calculates the factorial of a given number n using recursion.
-# The factorial of a non-negative integer n is the product of all positive integers less than or equal to n.
-# Factorial is defined as: n! = n * (n-1) * (n-2) * ... * 1, with the base case of 0! = 1.
-
-# Parameters:
-# n (int): The number for which the factorial is to be calculated. This should be a non-negative integer.
-#          The function expects an integer input from the user (from the command line argument).
-
-# Returns:
-# int: The factorial of the number n. If n is 0, the function returns 1 (the base case of factorial).
-#      Otherwise, it returns the result of n multiplied by the factorial of n-1 (recursive case).
-
 def factorial(n):
+    """
+    Fonction pour calculer le factoriel d'un nombre entier.
+
+    Description : 
+        Cette fonction utilise une approche récursive pour calculer le 
+        factoriel d'un nombre entier positif n. Le factoriel de n (noté n!) 
+        est le produit de tous les entiers de 1 à n.
+
+    Paramètres :
+        n (int) : Le nombre entier pour lequel nous voulons calculer le 
+                  factoriel.
+
+    Retour :
+        int : Le factoriel de n. Si n est égal à 0, la fonction retourne 1 
+              (par convention, 0! = 1).
+    """
     if n == 0:
         return 1
     else:
         return n * factorial(n - 1)
 
-# Read the input number from the command line argument
+# Vérification de l'argument passé via la ligne de commande
 f = factorial(int(sys.argv[1]))
 
-# Print the result of the factorial calculation
+# Affichage du résultat du calcul du factoriel
 print(f)
 
